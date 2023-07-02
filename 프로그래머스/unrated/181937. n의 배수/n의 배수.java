82d94b31143436e80a3a28242473d53e8c5abbe3
Jhsol <133485391+Jhsol3077@users.noreply.github.com>
2023-07-02 13:12:50 +0900

    [unrated] Title: n의 배수, Time: 0.01 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/unrated/181937. n의 배수/n의 배수.java
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -2,11 +2,9 @@ class Solution {
     public int solution(int num, int n) {
         if(2<=num && num<=100 && 2<=n && n<=9){
         if(num%n==0) {
-            System.out.println(num + "은 " + n + "의 배수이므로 1을 return합니다.");
             return 1;
         }
         else {
-            System.out.println(num + "은 " + n + "의 배수가 아니므로 0을 return합니다.");
             return 0;
         }
     }
